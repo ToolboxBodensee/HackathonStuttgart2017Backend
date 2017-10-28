31cd8048b6d32512611780d5dc6f19336399b0e4
Michael Malura <maluramichael@googlemail.com>
2017-10-28 09:14:13 +0200

    Move user by its direction

--- socket.js
+++ socket.js
@@ -1,13 +1,16 @@
 const R = require('ramda');
 
+// config
 const WIDTH = 1280;
 const HEIGHT = 768;
+const PIXEL_PER_TICK = 100;
 
 // List of userPoints { 'aaaa': [{x,y,direction},{x,y,direction},{x,y,direction}], ...}
 let points = {};
 
-// last tick
+// 
 let lastTick = null;
+
 
 module.exports = function configureSocketIO(io) {
   //********************************************************************************
@@ -28,11 +31,11 @@ module.exports = function configureSocketIO(io) {
       x: Math.random() * WIDTH,
       y: Math.random() * HEIGHT
     }
-    const direction = 0;
-    points[id].push({
+    const direction = Math.random() * 360;
+    points[id] = {
       position,
       direction
-    });
+    };
 
     // Notify everyone about the new player and his position
     someoneJoined(id, position, direction);
@@ -82,15 +85,37 @@ module.exports = function configureSocketIO(io) {
   }
 
   function tick() {
-    // 
-
     let delta = 0;
     if (lastTick) {
-      delta = Date.now() - lastTick;
+      delta = (Date.now() - lastTick) / 1000.0;
     }
-    lastTick = Date.now()
 
-    io.emit('tick', {lastTick, delta});
+    const diffs = {};
+
+    R.forEachObjIndexed(function (element, key) {
+      // Calculate x,y vec from angle      
+      const dx = Math.sin(element.direction);
+      const dy = Math.sin(element.direction);
+
+      // Translate x,y 
+      const tx = element.position.x + (PIXEL_PER_TICK * dx * delta);
+      const ty = element.position.y + (PIXEL_PER_TICK * dy * delta);
+
+      // Apply translation 
+      element.position.x = tx;
+      element.position.y = ty;
+
+      // Add element in diff object for later client update
+      diffs[key] = element;
+
+    }, points);
+
+    io.emit('tick', {
+      lastTick,
+      delta,
+      diffs
+    });
+    lastTick = Date.now()
   }
 
   //********************************************************************************
